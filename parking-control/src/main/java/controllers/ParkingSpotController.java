a04fdabb71dbdc03fccd1582d1bd53bf6d93a436
Maíra N. Rocha <mairanrocha@hotmail.com>
2023-09-19 17:45:16 -0300

    Criadas classe DTO e pacote DTO

--- parking-control/src/main/java/controllers/ParkingSpotController.java
+++ parking-control/src/main/java/controllers/ParkingSpotController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import services.ParkingSpotService;
 
 @RestController
-@CrossOrigin (origins = "*" , maxAge=3600) //????
+@CrossOrigin (origins = "*" , maxAge=3600) //mecanismo usado pra add cabeçalhos http que informam aos navegadores que permitam que uma aplicação web seja executada em uma origem e acesse recursos de outra origem. 
 @RequestMapping("/parking-spot")		//definindo a URI a nível de classe -> mapeando a rota (pense laravel)
 
 public class ParkingSpotController {
